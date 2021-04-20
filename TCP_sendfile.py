8501eaad101048c8b168b8900fe6ed29f36792c2
Sohyeon <sohyeonpark94@gmail.com>
2021-04-20 17:43:33 +0900

    20.Apr.21 Socket-mod

--- TCP_sendfile.py
+++ TCP_sendfile.py
@@ -8,13 +8,14 @@ data_socket, client_addr = sock.accept()
 print('Connection from', client_addr)
 msg = data_socket.recv(1024)
 print(msg.decode())
-file_name = input('File name to send (ex - D:/PyCharmWorkspace/test.txt) : ')
+file_name = input('File name to send (ex - test.txt) : ')
 print(f"Sending '{file_name}'")
-fn = file_name.split('/')
+plusdir = "D:/PyCharmWorkspace/test/send/" + file_name
+fn = plusdir.split('/')
 
 data_socket.sendall(fn[-1].encode())
 try:
-    with open(file_name, 'rb') as f:
+    with open(plusdir, 'rb') as f:
         data_socket.sendfile(f, 0)
     print('Sending complete')
 
